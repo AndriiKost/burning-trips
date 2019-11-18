21e529bc510731eade58997105f780fb213a557b
Andrii Kost <andrii@Andriis.local>
2019-11-17 19:09:39 -0600

    router auth guard bug fix

--- burning-client/src/router/middleware.ts
+++ burning-client/src/router/middleware.ts
@@ -1,18 +1,12 @@
 import { Route } from 'vue-router';
 import store from '@/store';
 
-export function loggedInGuard(to: Route, from: Route, next: any) {
-    store.dispatch('auth/initSession').then(success => {
-        if (!success && !to.matched.some(r => r.meta.allowGuest)) {
-            next({
-                path: '/login',
-                params: { redirectTo: to.fullPath }
-            });
-        } else if (success && to.name === 'Login') {
-            const path = to.params.redirectTo || '/';
-            next({ path });
-        } else {
-            next();
-        }
-    })
+export async function loggedInGuard(to: Route, from: Route, next: any) {
+    const isLoggedIn = await store.dispatch('auth/initSession')
+    if (!isLoggedIn && to.path !== '/login' && !to.matched.some(r => r.meta.allowGuest))  {
+        next('/login');
+    } else {
+        next();
+    }
+    // TODO: redirect after logging ing
 }
